514dfb8deeef59bf49a9c5ecdf81e3043d3cc213
Alausa AbdulAzeez <alausaabdulazeez@gmail.com>
2022-01-07 20:31:15 +0100

    dynamic user profile

--- client/pages/home/home.js
+++ client/pages/home/home.js
@@ -1,3 +1,18 @@
+const userProfile = document.querySelector(".user");
+const user = JSON.parse(localStorage.getItem("user"));
+
+userProfile.innerHTML = `<img
+              src=${
+                user.profile_picture === "test" || null
+                  ? "https://www.kindpng.com/picc/m/22-223863_no-avatar-png-circle-transparent-png.png"
+                  : user.profile_picture
+              }
+              alt=""
+              class="userImg"
+            />
+            <h3 class="username">${user.email}</h3>
+            `;
+
 const handleNavToProducts = () => {
   window.location.assign(
     "http://127.0.0.1:5500/client/pages/products/products.html"
