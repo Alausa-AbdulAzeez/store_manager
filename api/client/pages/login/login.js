89c6b24e17ac581381841ef2a0b9a38fc2299326
Alausa AbdulAzeez <alausaabdulazeez@gmail.com>
2022-01-10 08:33:03 +0100

    modified the uri

--- api/client/pages/login/login.js
+++ api/client/pages/login/login.js
@@ -20,7 +20,7 @@ const handleLogin = async (e) => {
 
   if (e.target.name === "loginBtn") {
     const response = await fetch(
-      "https://zstore-manager.herokuapp.com/api/auth/login",
+      "https://fast-ridge-96854.herokuapp.com/https://zstore-manager.herokuapp.com/api/auth/login",
       {
         method: "post",
         headers: { "Content-Type": "application/json" },
--- api/index.js
+++ api/index.js
@@ -22,7 +22,7 @@ app.use("/api/users/", userRoute);
 app.use(express.static(path.join(__dirname, "/client/")));
 
 app.get("*", (req, res) => {
-  res.sendFile(path.join(__dirname, "/client/pages/home/", "home.html"));
+  res.sendFile(path.join(__dirname, "/client/", "index.html"));
 });
 
 // const aaa = path.resolve(__dirname, "client/pages/home/home.js");
