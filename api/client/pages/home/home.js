8c8b190f775be9c738401384c7d05ad18d6d6f1b
Alausa AbdulAzeez <alausaabdulazeez@gmail.com>
2022-01-13 13:39:54 +0100

    modified fns

--- api/client/pages/home/home.js
+++ api/client/pages/home/home.js
@@ -1,19 +1,21 @@
-const user = JSON.parse(localStorage.getItem("user"));
-!user && window.location.assign("/pages/login/login.html");
+// const user = JSON.parse(localStorage.getItem("user"));
+// !user && window.location.assign("/pages/login/login.html");
 
 const userProfile = document.querySelector(".user");
+const prod = document.querySelector(".products");
+const attendants = document.querySelector(".attendants");
 
-userProfile.innerHTML = `<img
-              src=${
-                user.profile_picture === "test" || null
-                  ? "https://www.kindpng.com/picc/m/22-223863_no-avatar-png-circle-transparent-png.png"
-                  : user.profile_picture
-              }
-              alt=""
-              class="userImg"
-            />
-            <h3 class="username">${user.email}</h3>
-            `;
+// userProfile.innerHTML = `<img
+//               src=${
+//                 user.profile_picture === "test" || null
+//                   ? "https://www.kindpng.com/picc/m/22-223863_no-avatar-png-circle-transparent-png.png"
+//                   : user.profile_picture
+//               }
+//               alt=""
+//               class="userImg"
+//             />
+//             <h3 class="username">${user.email}</h3>
+//             `;
 
 const handleNavToProducts = () => {
   window.location.assign("/client/pages/products/products.html");
@@ -31,4 +33,9 @@ const handleLogout = () => {
   localStorage.removeItem("user");
 };
 
-window.addEventListener("load", checkUser());
+if (prod) {
+  attendants.addEventListener("click", handleNavToProducts);
+}
+if (attendants) {
+  attendants.addEventListener("click", handleNavToAttendants);
+}
